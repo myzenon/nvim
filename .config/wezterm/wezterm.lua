679f3b08a7bb429e802f75ce536f7287e28a7466
Boss <boss@odds.team>
2023-11-24 22:43:03 +0700

    Update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,9 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local mux = wezterm.mux
 
-wezterm.on('gui-startup', function(cmd) -- set startup Window position
-    local tab, pane, window = mux.spawn_window(cmd or
-        { position = { x = "10%", y = "10%" }, origin = "ActiveScreen" }
-    )
+wezterm.on("gui-startup", function(cmd) -- set startup Window position
+	local tab, pane, window = mux.spawn_window(cmd or { position = { x = "10%", y = "10%" }, origin = "ActiveScreen" })
 end)
 
 -- This table will hold the configuration.
@@ -14,7 +12,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -22,24 +20,24 @@ end
 -- Tabs
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
-config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE|MACOS_FORCE_ENABLE_SHADOW'
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE|MACOS_FORCE_ENABLE_SHADOW"
 
 -- Padding
 config.window_padding = {
-    left = 32,
-    right = 32,
-    top = 64,
-    -- top = 16,
-    bottom = 16,
+	left = 32,
+	right = 32,
+	top = 64,
+	-- top = 16,
+	bottom = 16,
 }
 
 config.max_fps = 240
-config.front_end = 'WebGpu'
+config.front_end = "WebGpu"
 -- config.use_ime = true
 -- config.key_map_preference = 'Physical'
 
 -- Fonts
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 -- config.harfbuzz_features = {
 --     "cv06=1",
 --     "cv14=1",
@@ -48,38 +46,38 @@ config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 --     "ss07=1",
 --     "ss09=1",
 -- }
-config.font = wezterm.font_with_fallback {
-    'MesloLGS Nerd Font',
-    -- 'PK Maehongson',
-    -- 'Sukhumvit Set',
-}
+config.font = wezterm.font_with_fallback({
+	"MesloLGS Nerd Font",
+	-- 'PK Maehongson',
+	-- 'Sukhumvit Set',
+})
 -- ซ้ำซ้อน
 config.font_size = 14.0
 
 -- Background
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 32
-config.window_close_confirmation = 'NeverPrompt'
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 34
+config.window_close_confirmation = "NeverPrompt"
 config.native_macos_fullscreen_mode = true
 config.window_frame = {
-    border_left_width = '0.1cell',
-    border_right_width = '0.1cell',
-    border_bottom_height = '0.1cell',
-    border_top_height = '0.1cell',
-    border_left_color = '#333333',
-    border_right_color = '#333333',
-    border_bottom_color = '#333333',
-    border_top_color = '#333333',
+	border_left_width = "0.1cell",
+	border_right_width = "0.1cell",
+	border_bottom_height = "0.1cell",
+	border_top_height = "0.1cell",
+	border_left_color = "#333333",
+	border_right_color = "#333333",
+	border_bottom_color = "#333333",
+	border_top_color = "#333333",
 }
 
 -- For example, changing the color scheme:
 -- config.color_scheme = 'AdventureTime'
 
 config.keys = {
-    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-    { key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
-    -- Make Option-Right equivalent to Alt-f; forward-word
-    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 }
 
 -- and finally, return the configuration to wezterm
