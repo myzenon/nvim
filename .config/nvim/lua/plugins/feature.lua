650241afafdf8fa53ab7f04c2c0c11989eeb7cfb
Boss <boss@odds.team>
2024-10-18 22:15:33 +0700

    feat(nvim): add text case

--- .config/nvim/lua/plugins/feature.lua
+++ .config/nvim/lua/plugins/feature.lua
@@ -185,7 +185,24 @@ return {
     end,
     cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
   },
-  -- {
-  --   "jamestthompson3/nvim-remote-containers",
-  -- },
+  {
+    "johmsalas/text-case.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("textcase").setup({})
+      require("telescope").load_extension("textcase")
+    end,
+    keys = {
+      "ga",
+      { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+    },
+    cmd = {
+      "Subs",
+      "TextCaseOpenTelescope",
+      "TextCaseOpenTelescopeQuickChange",
+      "TextCaseOpenTelescopeLSPChange",
+      "TextCaseStartReplacingCommand",
+    },
+    lazy = false,
+  },
 }
