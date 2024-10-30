bd1992c1f46bf80bed5521fad6d7b1878734e20d
Boss <boss@odds.team>
2024-10-30 23:03:56 +0700

    update

--- .config/nvim/lua/plugins/move.lua
+++ .config/nvim/lua/plugins/move.lua
@@ -13,10 +13,10 @@ return {
     build = "make",
     config = function()
       local moveline = require("moveline")
-      vim.keymap.set("n", "<M-k>", moveline.up)
-      vim.keymap.set("n", "<M-j>", moveline.down)
-      vim.keymap.set("v", "<M-k>", moveline.block_up)
-      vim.keymap.set("v", "<M-j>", moveline.block_down)
+      vim.keymap.set("n", "<M-K>", moveline.up)
+      vim.keymap.set("n", "<M-J>", moveline.down)
+      vim.keymap.set("v", "<M-K>", moveline.block_up)
+      vim.keymap.set("v", "<M-J>", moveline.block_down)
     end,
   },
   {
