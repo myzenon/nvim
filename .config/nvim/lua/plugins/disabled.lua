3586413fb0e82e81a305906039bc4b9cd774cb09
Boss <boss@odds.team>
2024-11-19 22:40:25 +0700

    update

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,4 @@
 return {
-  { "nvimdev/dashboard-nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "folke/which-key.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
